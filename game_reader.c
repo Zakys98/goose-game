915df48f4da14eb4d92a787a52824eb5dff695c3
Zakys98 <zakys98@gmail.com>
2021-02-09 20:20:19 +0100

    game game_reader changes

--- game_reader.c
+++ game_reader.c
@@ -9,10 +9,20 @@
  * @copyright GNU Public License
  */
 
-#include <stdio.h>
-
 #include "game_reader.h"
 
+STATUS game_create_from_file(Game* game, char* filename) {
+    if (game_create(game) == ERROR)
+        return ERROR;
+
+    if (game_load_spaces(game, filename) == ERROR)
+        return ERROR;
+
+    game_set_player_location(game, game_get_space_id_at(game, 0));
+    game_set_object_location(game, game_get_space_id_at(game, 0));
+
+    return OK;
+}
 
 STATUS game_load_spaces(Game* game, char* filename) {
     FILE* file = NULL;
