07fb31fc9ab3544d8432adc98f1271431c472247
Zakys98 <zakys98@gmail.com>
2021-02-26 15:37:52 +0100

    comments player

--- player.h
+++ player.h
@@ -1,11 +1,10 @@
 /** 
- * @brief It defines the game interface
- * for each command
+ * @brief It defines player interface
  * 
- * @file game.h
- * @author Profesores PPROG
+ * @file player.h
+ * @author Jiri Zak
  * @version 1.0 
- * @date 13-01-2015 
+ * @date 12-02-2021 
  * @copyright GNU Public License
  */
 
@@ -16,6 +15,13 @@
 #include "space.h"
 #include "types.h"
 
+/**
+ * Structure for Player
+ * Id id
+ * char[] name
+ * id location 
+ * Object *obj
+ */
 typedef struct player {
     Id id;
     char name[WORD_SIZE + 1];
@@ -23,16 +29,127 @@ typedef struct player {
     Object *obj;
 } Player;
 
+/**
+ * @brief Player create
+ *
+ * @author Jiri Zak
+ * @date 12-02-2021
+ * 
+ * @param id id of player
+ * @return pointer to Player
+ */
 Player*     player_create(Id);
+
+/**
+ * @brief Player destroy
+ *
+ * @author Jiri Zak
+ * @date 12-02-2021
+ * 
+ * @param p double pointer to player
+ * @return STATUS ERROR = 0, OK = 1
+ */
 STATUS      player_destroy(Player **);
+
+/**
+ * @brief Check if player is not NULL
+ *
+ * @author Jiri Zak
+ * @date 12-02-2021
+ * 
+ * @param p pointer to Player
+ * @return STATUS ERROR = 0, OK = 1
+ */
 STATUS      player_exist(Player *);
+
+/**
+ * @brief Player id getter 
+ *
+ * @author Jiri Zak
+ * @date 12-02-2021
+ * 
+ * @param p pointer to Player
+ * @return Id or -1 if ERROR
+ */
 Id          player_get_id(Player *);
+
+/**
+ * @brief Player name getter 
+ *
+ * @author Jiri Zak
+ * @date 12-02-2021
+ * 
+ * @param p pointer to Player
+ * @return pointer to const char or NULL if ERROR 
+ */
 const char* player_get_name(Player *);
+
+/**
+ * @brief Player name setter 
+ *
+ * @author Jiri Zak
+ * @date 12-02-2021
+ * 
+ * @param p pointer to Player
+ * @param str pointer to const char
+ * @return STATUS ERROR = 0, OK = 1
+ */
 STATUS      player_set_name(Player *, const char *);
+
+/**
+ * @brief Player location setter 
+ *
+ * @author Jiri Zak
+ * @date 12-02-2021
+ * 
+ * @param p pointer to Player
+ * @param id id of location
+ * @return STATUS ERROR = 0, OK = 1
+ */
 STATUS      player_set_location(Player *, Id);
+
+/**
+ * @brief PLayer location getter 
+ *
+ * @author Jiri Zak
+ * @date 12-02-2021
+ * 
+ * @param p pointer to Player
+ * @return Id or NO_ID if ERROR 
+ */
 Id      	player_get_location(Player *);
+
+/**
+ * @brief Player object setter 
+ *
+ * @author Jiri Zak
+ * @date 12-02-2021
+ *
+ * @param p pointer to Player 
+ * @param o pointer to Object
+ * @return STATUS ERROR = 0, OK = 1
+ */
 STATUS      player_set_object(Player *, Object *);
+
+/**
+ * @brief Player object getter 
+ *
+ * @author Jiri Zak
+ * @date 12-02-2021
+ * 
+ * @param p pointer to Player
+ * @return pointer to object or NULL if ERROR 
+ */
 Object*     player_get_object(Player *);
+
+/**
+ * @brief Player print
+ *
+ * @author Jiri Zak
+ * @date 12-02-2021
+ * 
+ * @param p pointer to Player
+ */
 void        player_print(Player *);
 
 #endif
