adda5743faec72f6a1429d87475d27f566bfca26
LittleHaku <ivandelhorno@gmail.com>
2021-04-07 01:48:16 -0700

    Inventory

--- include/inventory.h
+++ include/inventory.h
@@ -25,7 +25,7 @@ typedef struct _Inventory Inventory;
  * @param cap capacity of backpack
  * @return pointer to created inventory or NULL in case of error
  */
-Inventory* inventory_create(int); 
+Inventory *inventory_create(int cap);
 
 /**
  * @brief destructor for inventory
@@ -36,12 +36,57 @@ Inventory* inventory_create(int);
  * @param i double pointer to inventory
  * @return STATUS ERROR = 0, OK = 1
  */
-void inventory_destroy(Inventory**);
+void inventory_destroy(Inventory *i);
 
-BOOL inventory_isFull(Inventory*);
-STATUS inventory_set(Inventory*, Object*);
-Object* inventory_get(Inventory*, Id);
-STATUS inventory_add(Inventory*, Object*);
+/**
+ * @brief Checks if the given inventory is full
+ * 
+ * @param i Pointer to the inventory to be checked
+ * @return TRUE if it is full, NULL if it is not
+ */
+BOOL inventory_isFull(Inventory *i);
+
+/**
+ * @brief Checks if the given inventory is full
+ * 
+ * @param i Pointer to the inventory to be checked
+ * @return TRUE if it is empty, NULL if it is not
+ */
+BOOL inventory_isEmpty(Inventory *i);
+
+/**
+ * @brief Adds an ID to the inventory
+ * @author Ivan del Horno
+ * @param i Pointer to the inventory
+ * @param id Id of object to add
+ * @return OK or ERROR
+ */
+STATUS inventory_set_id(Inventory *i, Id id);
+
+/**
+ * @brief Deletes an object from the inventory given its Id
+ * @author Ivan del Horno
+ * @param i Pointer to the inventory
+ * @param id Id of the object to delete
+ * @return OK or ERROR
+ */
+STATUS inventory_del_id(Inventory *i, Id id);
+
+/**
+ * @brief Get the number of objects inside an inventory
+ * @author Ivan del Horno
+ * @param i Pointer to the inventory
+ * @return Number of objects inside the inventory or -1 in case of error
+ */
+int inventory_get_nObjects(Inventory *i);
+
+/**
+ * @brief Get the max number of objects inside a set
+ * @author Ivan del Horno
+ * @param i Pointer to the inventory
+ * @return Int number of max objects or -1 in case of error
+ */
+int inventory_get_capacity(Inventory *i);
 
 /**
  * @brief prints the contents of a specified inventory
@@ -53,7 +98,6 @@ STATUS inventory_add(Inventory*, Object*);
  * @param f file pointer
  * @return the number of printed characters or -1 in case of error
  */
-int inventory_print(Inventory*, FILE*);
-
+int inventory_print(Inventory *inv, FILE *f);
 
 #endif
--- src/inventory.c
+++ src/inventory.c
@@ -2,7 +2,7 @@
  * @brief Implementation of inventory
  *
  * @file inventory.c
- * @author Eva Moresova
+ * @author Eva Moresova and Ivan del Horno
  * @version 1.0
  * @date 22-03-2021
  * @copyright GNU Public License
@@ -13,77 +13,167 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-struct _Inventory {
-	Set* objects;
-	int capacity;
+#define MIN_CAP_INV 1
+
+struct _Inventory
+{
+	Set *objects; //!<Pointer to the set of items
+	int capacity; //!<Capacity of the inventory
 };
 
 // Private functions
 
+//Should this be private or public?
+/**
+ * @brief Checks if the inventory contains a given ID
+ * @author Ivan del Horno
+ * @param i Pointer to the inventory to be checked
+ * @param id ID of the object to search
+ * @return TRUE if it is conatained, FALSE if it is not
+ */
+BOOL _inventory_contains_id(Inventory *i, Id id);
+
+BOOL _inventory_contains_id(Inventory *i, Id id)
+{
+	int i, n;
+	Id *ids = NULL;
+
+	if (i == NULL || id == NO_ID)
+	{
+		return FALSE;
+	}
+
+	n = set_get_size(i->objects);
+	ids = set_get_elements(i->objects);
+
+	if (ids == NULL)
+	{
+		return FALSE;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		if (ids[i] == id)
+		{
+			return TRUE;
+		}
+	}
+
+	return FALSE;
+}
 
 // Implementation
 
-Inventory* inventory_create(int cap) {
-	if (cap <= 0) return NULL;
+Inventory *inventory_create(int cap)
+{
+	Inventory *i = NULL;
 
-	Inventory* i = malloc(sizeof(struct _Inventory));
-	if (i != NULL) {
-		i->capacity = cap;
-		i->objects = set_create();
-		if (i->objects == NULL)
-			return NULL;
+	if (cap < MIN_CAP_INV)
+		return NULL;
+
+	i = (Inventory *)malloc(sizeof(Inventory));
+	if (i == NULL)
+	{
+		return NULL;
 	}
+
+	i->objects = set_create();
+	if (i->objects == NULL)
+	{
+		free(i);
+		return NULL;
+	}
+
+	i->capacity = cap;
+
 	return i;
-} 
+}
 
-void inventory_destroy(Inventory** i) {
-	if (i == NULL || *i == NULL)
+void inventory_destroy(Inventory *i)
+{
+	if (i == NULL)
+	{
 		return;
-	set_destroy(&(*i)->objects);
-	free(*i);
-	*i = NULL;
+	}
+	set_destroy(i->objects);
+	free(i);
+	return;
 }
 
-BOOL inventory_isFull(Inventory* i) {
-	if (set_get_size(i->objects) == i->capacity) 
+BOOL inventory_isFull(Inventory *i)
+{
+	if (i == NULL)
+	{
 		return TRUE;
+	}
 
+	if (set_get_size(i->objects) == i->capacity)
+	{
+		return TRUE;
+	}
 	return FALSE;
 }
 
-BOOL inventory_isEmpty(Inventory* i) {
-	
-	if (set_get_size(i->objects) == i->capacity) 
+BOOL inventory_isEmpty(Inventory *i)
+{
+	if (i == NULL)
+	{
 		return TRUE;
+	}
 
-	return FALSE;
+	return (set_is_empty(i->objects) == TRUE);
 }
 
-// STATUS inventory_set(Inventory* i, Object* o) {
-// 	if (i == NULL || o == NULL) return ERROR;
-
-// 	return OK;
-// }
-
-Object* inventory_get(Inventory* i, Id id) {
-	if(i == NULL) return NULL;
-
-	if (set_delete(i->objects, id) == ERROR) return NULL;
-	//return game_get_object(g, id);
-	return NULL;
-}
-
-STATUS inventory_add(Inventory* i, Object* o) {
-	if (i == NULL || o == NULL || inventory_isFull(i)) 
+STATUS inventory_set_id(Inventory *i, Id id)
+{
+	if (i == NULL || id == NO_ID || inventory_isFull(i) == TRUE)
+	{
 		return ERROR;
+	}
 
-	return set_add(i->objects, object_get_id(o));
+	return set_add(i->objects, id);
 }
 
-int inventory_print(Inventory* i, FILE* f) { 
-	if (i == NULL) return -1;
+STATUS inventory_del_id(Inventory *i, Id id)
+{
+	if (i == NULL)
+	{
+		return ERROR;
+	}
 
-	int c = fprintf(f, "Inventory: ");
-	c += set_print(i->objects, f);
+	return (set_delete(i->objects, id));
+}
+
+int inventory_get_nObjects(Inventory *i)
+{
+	if (i == NULL)
+	{
+		return -1;
+	}
+
+	return set_get_size(i->objects);
+}
+
+int inventory_get_capacity(Inventory *i)
+{
+	if (i == NULL)
+	{
+		return -1;
+	}
+
+	return i->capacity;
+}
+
+int inventory_print(Inventory *inv, FILE *f)
+{
+	int c;
+
+	if (inv == NULL)
+	{
+		return -1;
+	}
+
+	c = fprintf(f, "Inventory: ");
+	c += set_print(inv->objects, f);
 	return c;
 }
