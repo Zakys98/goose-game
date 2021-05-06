13488f9f7c3b138bbcc01e9833d4bcfffff290a8
LittleHaku <ivandelhorno@gmail.com>
2021-05-06 18:05:18 +0200

    Dialogue test

--- src/dialogue_test.c
+++ src/dialogue_test.c
@@ -23,11 +23,66 @@ void test_dialogue_no_cmd()
     STATUS st = OK;
     T_Command cmd = NO_CMD;
     char str[60] = "Introduce a command: ";
-    PRINT_TEST_RESULT(strcmp(dialogue_print(cmd, st, g), str) == 0);
+    char *str2 = dialogue_print(cmd, st, g);
+    PRINT_TEST_RESULT(strcmp(str2, str) == 0);
+    free(str2);
+    free(g);
 }
+
+void test_dialogue_game_null()
+{
+    Game *g = NULL;
+    STATUS st = OK;
+    T_Command cmd = NEXT;
+    char *str2 = dialogue_print(cmd, st, g);
+    PRINT_TEST_RESULT(str2 == NULL);
+    free(str2);
+}
+
+void test_dialogue_status_wrong()
+{
+    Game *g = game_init();
+    STATUS st = 3;
+    T_Command cmd = NO_CMD;
+    char *str2 = dialogue_print(cmd, st, g);
+    PRINT_TEST_RESULT(str2 == NULL);
+    free(str2);
+    free(g);
+}
+
+void test_dialogue_command_wrong()
+{
+    Game *g = game_init();
+    STATUS st = OK;
+    T_Command cmd = UNKNOWN;
+    char str[60] = "This is not a valid action. Try again.";
+    char *str2 = dialogue_print(cmd, st, g);
+    PRINT_TEST_RESULT(strcmp(str2, str) == 0);
+    free(str2);
+    free(g);
+}
+
+// void test_dialogue_repeated_wrong()
+// {
+//     Game *g = game_init();
+//     STATUS st = ERROR;
+//     T_Command cmd = TAKE;
+//     char str[60] = "You have done this before without success.";
+//     char *str2 = dialogue_print(cmd, st, g);
+//     free(str2);
+//     str2 = dialogue_print(cmd, st, g);
+//     PRINT_TEST_RESULT(strcmp(str2, str) == 0);
+//     free(str2);
+//     free(g);
+// }
 
 void test_all()
 {
+    test_dialogue_no_cmd();
+    test_dialogue_game_null();
+    test_dialogue_status_wrong();
+    test_dialogue_command_wrong();
+    //test_dialogue_repeated_wrong();
 
     PRINT_PASSED_PERCENTAGE;
 }
